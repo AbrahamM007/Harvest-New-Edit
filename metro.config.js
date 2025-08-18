5d8e579a7a1943ba2060115a7cda75eb9b6d3344
Abraham Mora-Tadeo <132838022+AbrahamM007@users.noreply.github.com>
2025-08-18 10:55:43 -0700

    Fix Metro config for Stripe web compatibility

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,6 @@ const config = getDefaultConfig(__dirname);
 // Enable CSS support for web
 config.resolver.assetExts.push('css');
 
+// Fix Stripe React Native web compatibility
+config.resolver.unstable_conditionNames = ['browser', 'require', 'react-native'];
 module.exports = config;
